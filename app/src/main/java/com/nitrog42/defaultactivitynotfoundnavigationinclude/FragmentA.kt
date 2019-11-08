c151fa1bfbf308ea534598796c9088a25557a89b
nitrog42 <nitrog42@gmail.com>
2019-11-08 10:24:39 +0100

    Initial commit

---
+++ app/src/main/java/com/nitrog42/defaultactivitynotfoundnavigationinclude/FragmentA.kt
@@ -0,0 +1,6 @@
+package com.nitrog42.defaultactivitynotfoundnavigationinclude
+
+import androidx.fragment.app.Fragment
+
+class FragmentA : Fragment(R.layout.fragment_a) {
+}
---
+++ submodule/src/main/java/com/nitrog42/submodule/FragmentB.kt
@@ -0,0 +1,6 @@
+package com.nitrog42.submodule
+
+import androidx.fragment.app.Fragment
+
+class FragmentB : Fragment(R.layout.fragment_b) {
+}
